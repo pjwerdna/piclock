82fb15c6a2384682db9148cc557fc67c1495040d
pjwerdna <pjwerdna@users.noreply.github.com>
2020-02-26 13:14:00 +0000

    Delete CalendarCredentials.py
    File isnt required

--- CalendarCredentials.py
+++
@@ -1,7 +0,0 @@
-This file was never required or used
-
-CLIENT_ID=' 123245456567-sdfsdfsfethryjrthwefwrghrtherger.apps.googleusercontent.com'
-CLIENT_SECRET='SSSSSSSSSSSSSSSSSSSSSSSSSSS'
-DEVELOPER_KEY='AAAAAAAAAAAAAAAAAAAAAAAAAAA'
-CALENDAR='me.here@gmail.com'
-WUG_KEY='123123123123'
